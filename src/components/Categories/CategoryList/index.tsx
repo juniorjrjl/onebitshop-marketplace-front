bced84853f2be9b72a498ad61777be9ecaa614d4
Junior <junior.jr.jl@gmail.com>
2023-07-23 17:15:17 -0300

    feat: criando o componente de lista de categorias

---
+++ src/components/Categories/CategoryList/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container, SeeMore, Title, TitleContainer } from "./styled";
+
+const CategoryList = () =>{
+    return(
+        <Container>
+            <TitleContainer>
+                <Title>Pets</Title>
+                <SeeMore>Ver mais</SeeMore>
+            </TitleContainer>
+        </Container>
+    )
+}
+
+export default CategoryList
--- src/screens/Categories/index.tsx
+++ src/screens/Categories/index.tsx
@@ -2,12 +2,17 @@ import React from "react"
 import { Container } from "./styled"
 import DefaultTitle from "../../components/common/DefaultTitle"
 import NavBar from "../../components/common/NavBar"
+import CategoryList from "../../components/Categories/CategoryList"
 
 const Categories = () => {
     return(
         <>
             <Container>
                 <DefaultTitle fontSize={20}>Todas as Categories</DefaultTitle>
+
+                <CategoryList />
+                <CategoryList />
+                <CategoryList />
             </Container>
             <NavBar />
         </>
