1da93d44de0312189434d2ef21133c12a03ff6c0
Junior <junior.jr.jl@gmail.com>
2023-07-23 19:11:10 -0300

    feat: conclusão da tela de categoria

--- src/components/Category/CategoryList/index.tsx
+++ src/components/Category/CategoryList/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Product } from "../../../screens/Categories";
 import { FlatList, ListRenderItem, View } from "react-native";
+import CategoryCard from "./CategoryCard";
 
 interface Props {
     products: Product[];
@@ -8,10 +9,7 @@ interface Props {
 
 const CategoryList = ({ products }: Props) =>{
     
-    const renderItem: ListRenderItem<Product> = () => 
-        <>
-            <View></View>
-        </>
+    const renderItem: ListRenderItem<Product> = ({ item }) => <CategoryCard product={item}/>
 
     return(
         <FlatList data={products} renderItem={renderItem} showsVerticalScrollIndicator={false} />
