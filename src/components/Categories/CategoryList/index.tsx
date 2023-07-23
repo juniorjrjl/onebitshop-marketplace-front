66b5ce1183e52494a01632f7242b9c9ce8751e24
Junior <junior.jr.jl@gmail.com>
2023-07-23 18:16:55 -0300

    feat: conclusão da tela de lista de categorias

--- src/components/Categories/CategoryList/index.tsx
+++ src/components/Categories/CategoryList/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Container, SeeMore, Title, TitleContainer } from "./styled";
+import { Category, Product } from "../../../screens/Categories";
+import { FlatList, ListRenderItem } from "react-native";
+import CategoryCard from "../CategoryCard/indext";
 
-const CategoryList = () =>{
+const CategoryList = ({ category }: Category) =>{
+    const renderItem: ListRenderItem<Product> = ({item}) =>  <CategoryCard product={item} key={item.id}/>
     return(
         <Container>
             <TitleContainer>
-                <Title>Pets</Title>
+                <Title>{category._id}</Title>
                 <SeeMore>Ver mais</SeeMore>
             </TitleContainer>
+            <FlatList data={category.product} renderItem={renderItem}
+                horizontal showsHorizontalScrollIndicator={false}/>
         </Container>
     )
 }
