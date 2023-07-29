3d81c316891ceeb53123d9928d5ac29621e79cce
Junior <junior.jr.jl@gmail.com>
2023-07-29 13:08:21 -0300

    feat: estruturando o título

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,11 +1,16 @@
 import React from "react"
-import { Container } from "./styled";
+import { Container, SubTitle, SubtitleContainer, Title } from "./styled";
 import BackIcon from "../../components/common/BackIcon";
 
 const Product = () =>{
     return(
         <Container>
             <BackIcon marginLeft={30}/>
+            <Title>Playstation 4 com 2 controles</Title>
+            <SubtitleContainer>
+                <SubTitle>Publicado em 10/04/23</SubTitle>
+                <SubTitle>Refice, PE</SubTitle>
+            </SubtitleContainer>
         </Container>
     );
 };
--- src/screens/Product/styled.ts
+++ src/screens/Product/styled.ts
@@ -7,4 +7,22 @@ export const Container = styled.ScrollView`
     flex: 1;
     background-color: ${({ theme }) => theme.colors.backgroundLight};
     padding-top: ${statusBarHeight}px;
-`
+`;
+
+export const Title = styled.Text`
+    font-size: 22px;
+    color: ${({ theme }) => theme.colors.primaryText};
+    margin: 0px 30px;
+    font-weight: bold;
+`;
+
+export const SubtitleContainer = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 0px 30px;
+`;
+
+export const SubTitle = styled.Text`
+    font-size: 24px;
+    color: ${({ theme }) => theme.colors.secondaryText};
+`;
