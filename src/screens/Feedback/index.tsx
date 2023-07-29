15fddb02d0241292eceee4f73c8e89e2791ba34b
Junior <junior.jr.jl@gmail.com>
2023-07-29 17:38:10 -0300

    feat: conclusão da tela de avaliações

--- src/screens/Feedback/index.tsx
+++ src/screens/Feedback/index.tsx
@@ -1,15 +1,34 @@
 import React from "react";
-import { Container } from "./styled";
+import { Container, FeedbackText } from "./styled";
 import DefaultTitle from "../../components/common/DefaultTitle";
 import ProfileInfo from "../../components/common/ProfileInfo";
 import NavBar from "../../components/common/NavBar";
+import { AirbnbRating } from "react-native-ratings";
+import DefaultButton from "../../components/common/DefaultButton";
+import { useNavigation } from "@react-navigation/native";
+import { PropsStack } from "../../routes";
+import { Alert } from "react-native";
 
 const Feedback = () => {
+
+    const navigation = useNavigation<PropsStack>()
+
     return(
         <>
             <Container>
                 <DefaultTitle fontSize={20}>Avaliar</DefaultTitle>
                 <ProfileInfo />
+                <FeedbackText>Avalie o vendedor</FeedbackText>
+                <AirbnbRating 
+                    selectedColor="#5F96ED"
+                    showRating={false}
+                    size={0}
+                    defaultRating={0}
+                    />
+                <DefaultButton buttonType="primary" marginVertical={80} buttonHandle={() => {
+                    navigation.goBack()
+                    Alert.alert("Obrigado pela sua avaliação")
+                    }}>Avaliar</DefaultButton>
             </Container>
             <NavBar />
         </>
