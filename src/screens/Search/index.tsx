d10476ceb3eb45b70107289ea294e9dc2c387ed6
Junior <junior.jr.jl@gmail.com>
2023-09-25 20:17:49 -0300

    feat: conclusão da tela de search

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,20 +1,41 @@
-import React from "react"
-import { Container } from "./styled"
+import React, { useEffect, useState } from "react"
+import { Container, NoResult } from "./styled"
 import Header from "../../components/common/Header"
 import NavBar from "../../components/common/NavBar"
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import { PropsNavigationStack } from "../../routes"
 import ProductList from "../../components/common/ProductList"
+import { Product } from "../../entities/Product"
+import searchService from "../../services/searchService"
+import Loader from "../Loader"
 
 type Props = NativeStackScreenProps<PropsNavigationStack, "Search">
 
 const Search = ({ route }: Props) =>{
+    const [data, setData] = useState<Product[]>([])
+    const [loading, setLoading] = useState<boolean>(true);
     const query = route?.params.query
     
+    const handleSearch = async () =>{
+        const { data } = await searchService.getSearchedProducts(query)
+
+        setData(data.products);
+        setLoading(false);
+    }
+
+    useEffect(() => { handleSearch() }, [query])
+
     return(
         <Container>
             <Header />
-            <ProductList />
+            { 
+                loading ? 
+                    (<Loader />) : 
+                    (data.length <= 0 ? 
+                        <NoResult>Produto(s) não encontrado(s)</NoResult> : 
+                        <ProductList handleGetProducts={handleSearch} products={data}/>
+                    )
+            }
             <NavBar />
         </Container>
     )
